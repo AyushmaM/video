f10ffe8fb74162a9667c7327262c11abbd9109a4
AyushmaM <ayushmamalla16@gmail.com>
2019-07-12 04:52:49 +0000

    course path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :courses,only: [:index, :show]
 	root 'static_pages#index' 
 	namespace :instructor do 
 		resources :courses, only: [:new, :create, :show]
