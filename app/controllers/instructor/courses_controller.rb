49811f8629c4e829f4e383471d43ded9195a6029
AyushmaM <ayushmamalla16@gmail.com>
2019-07-16 03:18:34 +0000

    new aws

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -5,11 +5,10 @@ class Instructor::CoursesController < ApplicationController
   def new
     @course = Course.new
   end
-
-  def create
+    def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
-      redirect_to instructor_course_path(@course)
+    redirect_to instructor_course_path(@course)
     else
       render :new, status: :unprocessable_entity
     end
@@ -19,12 +18,13 @@ class Instructor::CoursesController < ApplicationController
   end
 
   private
-
-  def require_authorized_for_current_course
+    
+    def require_authorized_for_current_course
     if current_course.user != current_user
-      render plain: "Unauthorized", status: :unauthorized 
+      render plain: "Unauthorized", status: :unauthorized
     end
   end
+
 
   helper_method :current_course
   def current_course
