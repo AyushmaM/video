e6622c03e839916ab1b8672ce3ced2a3183c212b
AyushmaM <ayushmamalla16@gmail.com>
2019-07-16 00:32:54 +0000

    show html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_14_201238) do
+ActiveRecord::Schema.define(version: 2019_07_16_002017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2019_07_14_201238) do
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_enrollments_on_course_id"
     t.index ["user_id", "course_id"], name: "index_enrollments_on_user_id_and_course_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.text "caption"
+    t.integer "course_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_images_on_course_id"
+    t.index ["user_id", "course_id"], name: "index_images_on_user_id_and_course_id"
   end
 
   create_table "lessons", force: :cascade do |t|
